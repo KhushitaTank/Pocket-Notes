22ec9f05dc4a4cdbc15848eaa082cd18b840d498
Khushita Tank <khushita.tank@gmail.com>
2023-12-13 03:59:32 +0530

    first commit

--- src/components/Side-Navbar/CreateNewTitle.js
+++ src/components/Side-Navbar/CreateNewTitle.js
@@ -44,20 +44,25 @@ export default function CreateNewTitle({
       let newGroup = [...createdGroupName];
       let newIntial = generateintial(name);
       let newName = name.charAt(0).toUpperCase() + name.slice(1).toString();
-
-      newGroup.push({
-        title: newName,
-        color: color,
-        intial: newIntial,
-        notes: [],
-      });
-      setCreatedGroupName(newGroup);
-      localStorage.setItem("Group Name", JSON.stringify(newGroup));
-      setState(false);
-      setName();
-      setColor();
-      setIsErrorColor();
-      setIsErrorGroupName();
+      let RepeatedName = newGroup.filter((i) => i.title.toLowerCase() == newName.toLowerCase());
+      
+      if (!RepeatedName.length) {
+        newGroup.push({
+          title: newName,
+          color: color,
+          intial: newIntial,
+          notes: [],
+        });
+        setCreatedGroupName(newGroup);
+        localStorage.setItem("Group Name", JSON.stringify(newGroup));
+        setState(false);
+        setName();
+        setColor();
+        setIsErrorColor();
+        setIsErrorGroupName();
+      } else {
+        setIsErrorGroupName("Group Name Already Exist")
+      }
     } else {
       if (!(name !== undefined) || !(name !== "")) {
         setIsErrorGroupName("Group Name Require");
